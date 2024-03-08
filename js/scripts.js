3f11a55e45db25afbb2da3de1d7da50e03ef0043
Kirielle Davis <160552612+KirielleDavis@users.noreply.github.com>
2024-03-08 15:44:12 -0800

    Update pokemonList
    Added four pokemon objects to array.
    Keys added: name, type, height, weight

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList =[{name: 'Zubat', type: '[poison, flying]', height: '0.8 m', weight: '7.5 kg'},
+{name: 'Pidgey', type: '[flying, normal]', height: '0.3 m', weight: '1.8 kg'},
+{name: 'Charmander', type: 'fire', height: '0.6 m', weight: '8.5 kg'},
+{name: 'Politoed', type: 'water', height: '1.1 m', weight: '33.9 kg'}
+]
