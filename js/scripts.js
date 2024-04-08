c6cfbac4a0b1334512dae8e7633ef12e465a0463
Kirielle Davis <160552612+KirielleDavis@users.noreply.github.com>
2024-04-08 12:36:35 -0700

    Display Pokemon list
    Created a for loop to display the Pokemon list on website.
    Added a string to show which one is the biggest.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
-let pokemonList =[{name: 'Zubat', type: '[poison, flying]', height: '0.8 m', weight: '7.5 kg'},
-{name: 'Pidgey', type: '[flying, normal]', height: '0.3 m', weight: '1.8 kg'},
-{name: 'Charmander', type: 'fire', height: '0.6 m', weight: '8.5 kg'},
-{name: 'Politoed', type: 'water', height: '1.1 m', weight: '33.9 kg'}
-]
+let pokemonList =
+[{
+  name: 'Zubat', 
+  type: '[poison, flying]', 
+  height: 0.8, 
+  weight: '7.5 kg'
+},
+{
+  name: 'Pidgey', 
+  type: '[flying, normal]', 
+  height: 0.3, 
+  weight: '1.8 kg'
+},
+{
+  name: 'Charmander', 
+  type: 'fire', 
+  height: 0.6, 
+  weight: '8.5 kg'
+},
+{
+  name: 'Politoed', 
+  type: 'water', 
+  height: 1.1, 
+  weight: '33.9 kg'
+}
+];
+//display pokemon and show which which one is the biggest
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' ' + "(height:" + ' ' + pokemonList[i].height + ') ');
+  if (pokemonList[i].height > 1) { 
+    document.write("- Wow, that's big!");
+  }
+  document.write('<br>');
+}
